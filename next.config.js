9a51d733dde1057d5d56ce81c642056ae62c4551
Julian <mayorga.julian@gmail.com>
2020-05-14 18:50:58 -0300

    Add codesandbox react and test examples

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
+const codesandbox = require("remark-codesandbox");
+
+const mode = "iframe";
+
 const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      [
+        codesandbox,
+        {
+          mode,
+          query:
+            mode === "iframe"
+              ? {
+                  fontsize: "14px",
+                  hidenavigation: 0,
+                  theme: "dark",
+                }
+              : undefined,
+          autoDeploy: false,
+          customTemplates: {
+            test: {
+              extends: "n9m2w9q8x0",
+              entry: "index.test.js",
+            },
+          },
+        },
+      ],
+    ],
+  },
 });
+
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "tsx", "md", "mdx"],
 });
