3132cd806e8c92c8ae9b811a9af157c5f1749631
Julian <mayorga.julian@gmail.com>
2020-05-14 17:51:59 -0300

    Write posts in MDX

--- components/Shell.tsx
+++ components/Shell.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { ThemeProvider } from "theme-ui";
+import { ThemeProvider, Box, Grid, Heading, Flex } from "theme-ui";
 import theme from "./theme";
 import Head from "next/head";
+import NextLink from "next/link";
 
 export default ({ children }) => (
   <ThemeProvider theme={theme}>
@@ -9,6 +10,40 @@ export default ({ children }) => (
       <title>refactoring</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    {children}
+    <Box sx={{ maxWidth: ["100%", 1200], margin: "0 auto", paddingTop: 4 }}>
+      <Grid
+        sx={{
+          gridAutoFlow: "column",
+          width: "fit-content",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        gap={2}
+      >
+        <Box
+          sx={{
+            width: 30,
+            height: 30,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <svg
+            style={{ height: "100%", width: "100%" }}
+            viewBox="0 0 100 100"
+            preserveAspectRatio="true"
+          >
+            <circle cx={50} cy={50} r={50} fill="currentColor" />
+          </svg>
+        </Box>
+        <NextLink href="/">
+          <Heading sx={{ cursor: "pointer" }}>refactoring</Heading>
+        </NextLink>
+      </Grid>
+      <Box as="main" sx={{ paddingTop: 4 }}>
+        {children}
+      </Box>
+    </Box>
   </ThemeProvider>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,25 @@
+import { Box, Link, Text } from "theme-ui";
+import NextLink from "next/link";
 import Shell from "../components/Shell";
-import { Box, Heading, Grid } from "theme-ui";
 
 export default function Home() {
   return (
     <Shell>
-      <Box>
-        <Box
-          as="main"
-          sx={{ maxWidth: ["100%", 1200], margin: "0 auto", paddingTop: 4 }}
-        >
-          <Grid
-            sx={{
-              gridAutoFlow: "column",
-              width: "fit-content",
-              alignItems: "center",
-            }}
-          >
-            <Box sx={{ width: 50, padding: 2 }}>
-              <svg viewBox="0 0 100 100" preserveAspectRatio="true">
-                <circle cx={50} cy={50} r={50} fill="currentColor" />
-              </svg>
-            </Box>
-            <Heading>refactoring</Heading>
-          </Grid>
-        </Box>
+      <Box marginTop={4}>
+        <Link>
+          <NextLink href="/counter-component">
+            <Text
+              sx={{
+                cursor: "pointer",
+                width: "fit-content",
+                fontSize: 3,
+                textDecoration: "underline",
+              }}
+            >
+              Counter component
+            </Text>
+          </NextLink>
+        </Link>
       </Box>
     </Shell>
   );
