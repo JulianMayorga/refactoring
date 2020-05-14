89807cda12948fa88497f3d2da5a0ad75fd0ec5c
Julian <mayorga.julian@gmail.com>
2020-05-14 19:08:27 -0300

    Red, green, refactor skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import { Box, Link, Text } from "theme-ui";
+import { Link, Text, Grid } from "theme-ui";
 import NextLink from "next/link";
 import Shell from "../components/Shell";
 
 export default function Home() {
   return (
     <Shell>
-      <Box marginTop={4}>
+      <Grid marginTop={4}>
+        <Link>
+          <NextLink href="/red-green-refactor">
+            <Text
+              sx={{
+                cursor: "pointer",
+                width: "fit-content",
+                fontSize: 3,
+                textDecoration: "underline",
+              }}
+            >
+              Red, green, refactor
+            </Text>
+          </NextLink>
+        </Link>
         <Link>
           <NextLink href="/counter-component">
             <Text
@@ -20,7 +34,7 @@ export default function Home() {
             </Text>
           </NextLink>
         </Link>
-      </Box>
+      </Grid>
     </Shell>
   );
 }
