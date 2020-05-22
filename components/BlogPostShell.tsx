9aeffe4ce62080df79032f1cf302cc55fdc68fda
Julian <mayorga.julian@gmail.com>
2020-05-22 18:34:37 -0300

    Use dark code highlight theme

--- components/BlogPostShell.tsx
+++ components/BlogPostShell.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Shell from "./Shell";
-import "prismjs/themes/prism.css";
+import "prismjs/themes/prism-tomorrow.css";
 
 export default function BlogPostShell({ children }) {
   return <Shell>{children}</Shell>;
