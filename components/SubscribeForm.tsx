15bbde2f398a8bce8b9e132b5013d9470bd13aae
Julian <mayorga.julian@gmail.com>
2020-05-24 22:36:24 -0300

    Style subscribe form

--- components/SubscribeForm.tsx
+++ components/SubscribeForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import { Text, Input, Button } from "theme-ui";
+import { Text, Input, Button, Grid } from "theme-ui";
 
 export function SubscribeForm() {
   return (
@@ -29,38 +29,45 @@ export function SubscribeForm() {
           target="_blank"
         >
           <div id="mc_embed_signup_scroll">
-            <label htmlFor="mce-EMAIL">
+            <label style={{ width: "100%" }} htmlFor="mce-EMAIL">
               <Text>Subscribe to get content from time to time</Text>
             </label>
-            <Input
-              type="email"
-              name="EMAIL"
-              className="email"
-              id="mce-EMAIL"
-              placeholder="email address"
-              required
-              style={{ marginRight: 10 }}
-            />
-            <div
-              style={{ position: "absolute", left: -5000 }}
-              aria-hidden="true"
-            >
-              <Input
-                type="text"
-                name="b_d44198b27d5fba22549f52339_b1b66796bb"
-                tabIndex={-1}
-              />
-            </div>
-            <div className="clear">
-              <Button
-                type="submit"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                sx={{ cursor: "pointer" }}
-              >
-                Subscribe
-              </Button>
-            </div>
+            <Grid columns={[1, "1fr auto", "1fr auto"]}>
+              <div>
+                <Input
+                  type="email"
+                  name="EMAIL"
+                  className="email"
+                  id="mce-EMAIL"
+                  placeholder="email address"
+                  required
+                  style={{ marginRight: 10 }}
+                />
+                <div
+                  style={{ position: "absolute", left: -5000 }}
+                  aria-hidden="true"
+                >
+                  <Input
+                    type="text"
+                    name="b_d44198b27d5fba22549f52339_b1b66796bb"
+                    tabIndex={-1}
+                  />
+                </div>
+              </div>
+              <div className="clear">
+                <Button
+                  type="submit"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  sx={{
+                    width: ["100%", "initial", "initial"],
+                    cursor: "pointer",
+                  }}
+                >
+                  Subscribe
+                </Button>
+              </div>
+            </Grid>
           </div>
         </form>
       </div>
